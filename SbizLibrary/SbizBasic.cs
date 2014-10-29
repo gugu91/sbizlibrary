8cf283fbba0bc97313ddb63b00a79433741b2e57
gugu91 <gporce@gmail.com>
2014-10-29 11:49:04 +0100

    mod2

---
+++ SbizLibrary/SbizBasic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace SbizLibrary
+{
+    public static class SbizBasic
+    {
+        public static byte[] SerializeObject(Object o)
+        {
+            if (o == null)
+                return null;
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, o);
+                return ms.ToArray();
+            }
+        }
+        public static Object DeserializeByteArray(byte[] array)
+        {
+            MemoryStream ms = new MemoryStream(array);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            ms.Position = 0;
+
+            Object o;
+
+            try
+            {
+                o = bf.Deserialize(ms);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return o;
+        }
+    }
+}
