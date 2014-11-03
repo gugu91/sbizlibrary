207eb9725584fddd2d2271e75e111eb59977be7e
gugu91 <gporce@gmail.com>
2014-11-03 16:57:43 +0100

    added sbizmouseeventargs

---
+++ SbizLibrary/SbizMouseEventArgs.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Sbiz.Library
+{
+    [Serializable]
+    class SbizMouseEventArgs
+    {
+        #region Attributes
+        private MouseButtons _button;
+        private int _clicks;
+        private int _delta;
+        private System.Drawing.Point _location;
+        private int _x;
+        private int _y;
+        #endregion
+
+        #region Properties
+        public MouseButtons Button
+        {
+            get
+            {
+                return _button;
+            }
+        }
+        public int Clicks
+        {
+            get
+            {
+                return _clicks;
+            }
+        }
+        public int Delta
+        {
+            get
+            {
+                return _delta;
+            }
+        }
+        public System.Drawing.Point Location
+        {
+            get
+            {
+                return _location;
+            }
+        }
+        public int X
+        {
+            get
+            {
+                return _x;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return _y;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+        public SbizMouseEventArgs(MouseButtons button, int clicks, int delta, System.Drawing.Point location, int x, int y)
+        {
+            _button = button;
+            _clicks = clicks;
+            _delta = delta;
+            _location = location;
+            _x = x;
+            _y = y;
+        }
+        #endregion
+
+        #region InstanceMethods
+        public byte[] ToByteArray()
+        {
+            return SbizBasic.SerializeObject(this);
+        }
+        #endregion
+    }
+}
